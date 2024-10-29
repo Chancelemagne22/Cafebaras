0188c02653f0cc3293a677cce4cd759dcc870f8f
Chance Justine <bragaischancejustine@gmail.com>
2024-10-29 13:31:20 +0800

    Modified Folders

--- cafebaras/src/frontend/App.jsx
+++ cafebaras/src/frontend/App.jsx
@@ -3,19 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import ItemManagement from './components/ItemManagement'
-import NotFound from './components/NotFound'; // Create this component for unmatched routes
+import ItemManagement from './components/ItemManagement';
+import NotFound from './components/NotFound';
+import ProtectedRoute from './components/loginComponents/protectedRoute';
 
 function App() {
     const [id, setID] = useState('');
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login setID={setID}/>} />
+                <Route path="/" element={<Login setID={setID} />} />
+                <Route 
+                    path="/dashboard" 
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard id={id} />
+                        </ProtectedRoute>
+                    } 
+                />
                 <Route path="/signup" element={<SignUp />} />
-                <Route path="/dashboard" element={<Dashboard id={id} />} />
                 <Route path="/items" element={<ItemManagement />} />
-                <Route path="*" element={<NotFound />} /> {/* Fallback route */}
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
