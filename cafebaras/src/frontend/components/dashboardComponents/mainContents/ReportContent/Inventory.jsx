e1cd905055001a9438b99954d92eb077144803ae
Calhanito <calhansantos@gmail.com>
2024-11-17 11:22:48 +0800

    para sa chart

--- cafebaras/src/frontend/components/dashboardComponents/mainContents/ReportContent/Inventory.jsx
+++ cafebaras/src/frontend/components/dashboardComponents/mainContents/ReportContent/Inventory.jsx
@@ -1,26 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { Chart } from 'primereact/chart';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import supabase from './supabaseAPI';
 
 function Inventory() {
     const [chartData, setChartData] = useState({});
     const [chartOptions, setChartOptions] = useState({});
+    const [stockedUnits, setStockedUnits] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+    const fetchStockedUnits = async () => {
+        setLoading(true); // Set loading to true at the start of fetch
+        let { data, error} = await supabase
+          .from('stocks')
+          .select('*');
+  
+        if (error) {
+          console.error("Error fetching Stocked Units:", error);
+        } else {
+          setStockedUnits(data);
+        }
+        setLoading(false); // Set loading to false after fetch
+      };
+
+      fetchStockedUnits();
+    }, []);
+
+    console.log(stockedUnits);
 
     useEffect(() => {
         const data = {
-            labels: ['A', 'B', 'C'],
+            labels: stockedUnits.map(row => row.Item_Name),
             datasets: [
                 {
-                    data: [540, 325, 702],
+                    data: stockedUnits.map(row => row.Stocked_Units),
                     backgroundColor: [
                         '#FCB025',
                         '#C67529',
                         '#FFE3A8'
                     ],
                     hoverBackgroundColor: [
-                        'white',
-                        'white',
-                        'white'
+                        '#EB8509',
+                        '#EB8509',
+                        '#EB8509'
                     ]
                 }
             ]
@@ -39,8 +63,6 @@ function Inventory() {
         setChartOptions(options);
     }, []);
 
-       
-
     return (
         <>
         <div id = "invText">Report for date period:</div>
@@ -50,13 +72,27 @@ function Inventory() {
                 <div className="card">
                     <div className="pieContainer">
                         <Chart type="pie" data={chartData} options={chartOptions} className="piechart" />
-                        <div>Quarterly Sales</div>
+                        <div className='Q_S_text'>Quarterly Sales</div>
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="table">table</div>
-            </div>
+                <div className="tables">
+                    <DataTable 
+                        className="stocks-table" 
+                        value={stockedUnits} 
+                        loading={loading} 
+                        paginator 
+                        rows={5} 
+                        rowsPerPageOptions={[5, 10, 25, 50]} 
+                        autoLayout={true}
+                    >
+                        <Column className="datas" field="itemID" header="Item Code"/>
+                        <Column className="datas" field="Item_Name" header="Item Name"/>
+                        <Column className="datas" field="Stocked_Units" header="Stocked Units"/>
+
+
+                    </DataTable>
+                </div>
         </div>
     </>
     )
