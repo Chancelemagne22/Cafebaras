0ab3b947af7dbfda7697e3afbc4aa90717c329de
Chance Justine <bragaischancejustine@gmail.com>
2024-12-26 08:57:13 +0800

    ReworkDrop

--- cafebaras/src/frontend/order.js
+++
@@ -1,9 +0,0 @@
-export {default as OrderManagement} from './OrderManagement'
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
